279c3c2ef114db70f977f438f3ca42373cd09d30
NotIllgyaz <81466032+NotIllgyaz@users.noreply.github.com>
2022-01-16 08:48:57 +0100

    1 line moved.

--- Controllers/ApiUrls.cs
+++ Controllers/ApiUrls.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-using MySql.Data.MySqlClient; 
+using MySql.Data.MySqlClient;
 using System.Data;
 using CattoIMGApi.Functions;
-    
+
+#pragma warning disable CS8604
+
 namespace CattoIMGApi.Controllers
 {
     [ApiController]
@@ -12,27 +14,23 @@ namespace CattoIMGApi.Controllers
         [Route("/api/getUser/")]
         public ActionResult AllVersions(string username)
         {
-            MySQLData mySQLData = new MySQLData(); // Load CattoIMGApi.Functions.MySQLData Variables      
-
             DataTable dt = new DataTable();
 
             var data = new List<Models.userData>();
-            var con = new MySqlConnection(mySQLData.mysqlConnection);
-            
+            var con = new MySqlConnection(new MySQLData().mysqlConnection);
+
             con.Open();
 
-            using (MySqlCommand cmd = new MySqlCommand("SELECT username, rankId FROM userData WHERE username = @UserName;", con))
-            {
-                cmd.Parameters.AddWithValue("@UserName", username);
+            MySqlCommand cmd = new MySqlCommand("SELECT username, rankId FROM userData WHERE username = @UserName;", con);
 
-                dt.Load(cmd.ExecuteReader());
-            };
+            cmd.Parameters.AddWithValue("@UserName", username);
 
+            dt.Load(cmd.ExecuteReader());
+            
             foreach (DataRow row in dt.Rows)
             {
                 string[] ranks = { "Member", "Developer", "Owner" };
 
-                #pragma warning disable CS8604 
                 var rank = Int32.Parse(row[1].ToString());
 
                 data.Add(new Models.userData { Username = row[0].ToString(), Rank = $"{ranks[rank]}", UserBio = "Feature soon." });
